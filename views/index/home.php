a4d6e1ee42800c651aa342b07735a981f99b2a46
MagiAndreeva <magito.andreeva@abv.bg>
2018-03-17 15:51:56 +0200

    move title and nav into header

--- views/index/home.php
+++ views/index/home.php
@@ -1,35 +1,4 @@
-<h1 class="site-heading text-center text-white d-none d-lg-block">
-    <span class="site-heading-upper text-primary mb-3">САЙТЪТ, КОЙТО ЩЕ ВИ ПОМОГНЕ ДА ИЗБЕРЕТЕ СВОЯТА БЪДЕЩА ДЕСТИНАЦИЯ.</span>
-    <span class="site-heading-lower">Опознай България</span>
-</h1>
 
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item active px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="index.html">Home
-                        <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="about.html">About</a>
-                </li>
-                <li class="nav-item px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="products.html">Products</a>
-                </li>
-                <li class="nav-item px-lg-4">
-                    <a class="nav-link text-uppercase text-expanded" href="store.html">Store</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
 
 <section class="page-section clearfix">
     <div class="container">
--- views/layouts/_header.php
+++ views/layouts/_header.php
@@ -23,3 +23,37 @@
 </head>
 
 <body>
+
+<h1 class="site-heading text-center text-white d-none d-lg-block">
+    <span class="site-heading-upper text-primary mb-3">САЙТЪТ, КОЙТО ЩЕ ВИ ПОМОГНЕ ДА ИЗБЕРЕТЕ СВОЯТА БЪДЕЩА ДЕСТИНАЦИЯ.</span>
+    <span class="site-heading-lower">Опознай България</span>
+</h1>
+
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item active px-lg-4">
+                    <a class="nav-link text-uppercase text-expanded" href="<?php echo BASE_URL."/index.php?page=index"; ?>">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item px-lg-4">
+                    <a class="nav-link text-uppercase text-expanded" href="about.html">About</a>
+                </li>
+                <li class="nav-item px-lg-4">
+                    <a class="nav-link text-uppercase text-expanded" href="products.html">Products</a>
+                </li>
+                <li class="nav-item px-lg-4">
+                    <a class="nav-link text-uppercase text-expanded" href="<?php echo BASE_URL."/index.php?page=login"; ?>">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
